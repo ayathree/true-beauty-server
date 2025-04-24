9aebbab94111f34027d504c8bb651a5b4ef04aa6
ayathree <nobanitaayathree333@gmail.com>
2025-04-25 00:42:07 +0600

    delete operation for my cart api

--- index.js
+++ index.js
@@ -322,6 +322,14 @@ async function run() {
       const result =await cartCollection.find(query).toArray()
       res.send(result)
     })
+    // delete a order data from db
+    app.delete('/cartData/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      
+      const result =await cartCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
 
